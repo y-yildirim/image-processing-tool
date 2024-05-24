02037197bc4f4dab3205783b26e840f5250c4aa4
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2024-05-24 16:39:26 +0300

    Update thresholding method to adaptive with different parameters

--- thresholding.py
+++ thresholding.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-def apply_threshold(image, method="binary", thresh=127, maxval=255):
+def apply_threshold(image, method="adaptive", thresh=127, maxval=255):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     if method == "binary":
         ret, thresh_img = cv2.threshold(gray, thresh, maxval, cv2.THRESH_BINARY)
@@ -11,7 +11,7 @@ def apply_threshold(image, method="binary", thresh=127, maxval=255):
         )
     elif method == "adaptive":
         thresh_img = cv2.adaptiveThreshold(
-            gray, maxval, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+            gray, maxval, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 5
         )
     else:
         raise ValueError("Unsupported thresholding method")

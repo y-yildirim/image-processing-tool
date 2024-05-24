7f72502d22a37458c5f5b985b5d4bc72a46a03ae
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2024-05-24 17:14:21 +0300

    Refactor morphology.py to improve morphological operations

--- morphology.py
+++ morphology.py
@@ -1,15 +1,16 @@
 import cv2
 
 
-def apply_morphology(image, operation="dilate", kernel_size=(5, 5)):
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
+def apply_morphology(image, operation="dilate", kernel_size=(7, 7)):
     if operation == "dilate":
-        return cv2.dilate(image, kernel, iterations=1)
+        return cv2.dilate(image, None, iterations=3)
     elif operation == "erode":
-        return cv2.erode(image, kernel, iterations=1)
+        return cv2.erode(image, None, iterations=3)
     elif operation == "open":
-        return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
+        return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations=1)
     elif operation == "close":
-        return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel_size)
+        return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=1)
     else:
         raise ValueError("Unsupported morphological operation")
